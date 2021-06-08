d1d63155fc619607e74b7b931e19f33056eb8bcd
engineer <Examle20@yandex.ru>
2021-06-08 23:13:01 +0300

    feat: сверстать компонент Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import {Header} from "../Header/Header";
 import {Promo} from "../Promo/Promo";
 import {AboutProject} from "../AboutProject/AboutProject";
+import {Techs} from "../Techs/Techs";
 
 export const Main = () => {
   return (
     <>
       <Promo />
       <AboutProject />
+      <Techs />
     </>
   );
 }
