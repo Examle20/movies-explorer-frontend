922418459521010922e674edd77e81336cef64a2
engineer <Examle20@yandex.ru>
2021-06-16 18:56:18 +0300

    fix: исправить отступы в main а 1280

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -5,7 +5,7 @@ import {Header} from "../Header/Header";
 export const Promo = () => {
   return (
     <section className='promo'>
-      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <h1 className="promo__title">Учебный проект студента факультета <br/>Веб-разработки.</h1>
       <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       <a className="promo__link" href="#about">Узнать больше</a>
       <div className="promo__image"/>
