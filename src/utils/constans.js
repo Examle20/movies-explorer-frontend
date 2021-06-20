b2eb03d19cd162cec14045277ffde99b4b2a5904
engineer <Examle20@yandex.ru>
2021-06-20 16:00:50 +0300

    feat: адаптировать компонент movies под 768

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,7 +1,7 @@
 import logo1 from "../images/img1.png"
 import logo2 from "../images/img2.png"
 export const allFilms = [
-  {src: logo1, title:"33 слова о дизайне", time:"1ч42м"},
+  {src: logo1, title:"Пи Джей Харви: A dog called money", time:"1ч42м"},
   {src: logo2, title:"33 слова о дизайне", time:"1ч42м"},
   {src: logo1, title:"33 слова о дизайне", time:"1ч42м"},
   {src: logo2, title:"33 слова о дизайне", time:"1ч42м"},
